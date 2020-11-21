cd1a7c5b4a0a6c79aa6885c24520b666f5a691df
Ted Peters <44210809+Drop-G@users.noreply.github.com>
2020-11-21 14:39:16 -0500

    Updates

--- src/utils/DataAreaContext.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-// components will more accessible, this isnt a great idea because it makes it harder to reuse components
-const DataAreaContext = React.createContext({
-
-});
-
-export default DataAreaContext;
