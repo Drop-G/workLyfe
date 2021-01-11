96bef66bef745743d4840c2fe0e12af15d139c10
Drop-G <tedpetersiv@gmail.com>
2021-01-11 17:18:27 -0500

    added readme and fixed bugs

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import Main from './components/Main';
-import Wrapper from './components/Wrapper';
-import Header from './components/Header';
-// import Loader from './components/spinner';
+import React from "react";
+import Main from "./components/Main";
+import Wrapper from "./components/Wrapper";
+import Header from "./components/Header";
 
+import "./App.css";
 
 function App() {
-    return (
-      <div className= "App loader">
-     <Wrapper>
-       <Header />
-       <Main />
-     </Wrapper>
-      </div>
-    );
+  return (
+    <div className="App">
+      <Wrapper>
+        <Header />
+        <Main />
+      </Wrapper>
+    </div>
+  );
 }
 
 export default App;
